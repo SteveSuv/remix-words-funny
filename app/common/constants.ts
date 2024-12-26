d5b1f5d131464cb8c6ef032e17742a9ec40cf1ef
zhangtaojun <zhangtaojun@leetcode.cn>
2024-12-26 13:13:37 +0800

    feat: add port

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -3,5 +3,7 @@ export const JWT_KEY = "ACCESS_TOKEN";
 export const IS_PROD = import.meta.env.PROD;
 
 const DEV_TRPC_URL = "http://localhost:3001/trpc";
+// when deploy to prod, change to real domain
 const PROD_TRPC_URL = "http://localhost:3001/trpc";
+
 export const TRPC_URL = IS_PROD ? PROD_TRPC_URL : DEV_TRPC_URL;
