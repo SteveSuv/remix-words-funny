520a0bff18c8a49249c34056662766849bae257a
zhangtaojun <zhangtaojun@leetcode.cn>
2024-12-31 15:23:47 +0800

    feat: add some code comments

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ docker rmi wordsfunny-image
 
 docker build --platform linux/amd64 -t wordsfunny-image .
 docker run --name wordsfunny-app -p 3001:3001 -d wordsfunny-image
-docker image prune -f
 
 sleep 3
 open http://localhost:3001
