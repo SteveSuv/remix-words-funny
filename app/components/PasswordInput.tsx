cbb4b596263b5d6a89326fd19d09f788c05bf989
zhangtaojun <2507698457@qq.com>
2025-05-16 20:54:23 +0800

    feat: upgrade packages

--- app/components/PasswordInput.tsx
+++ app/components/PasswordInput.tsx
@@ -1,35 +1,32 @@
 import { Button, Input, InputProps } from "@heroui/react";
 import { LuIcon } from "./LuIcon";
-import { forwardRef, Ref, useState } from "react";
+import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 
-export const PasswordInput = forwardRef(
-  (props: InputProps, ref: Ref<HTMLInputElement>) => {
-    const [showPassword, setShowPassword] = useState(false);
+export const PasswordInput = (props: InputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
 
-    return (
-      <Input
-        minLength={8}
-        maxLength={30}
-        label="密码"
-        placeholder="请输入密码"
-        type={showPassword ? "text" : "password"}
-        variant="bordered"
-        endContent={
-          <Button
-            variant="light"
-            size="sm"
-            isIconOnly
-            onPress={() => {
-              setShowPassword(!showPassword);
-            }}
-          >
-            <LuIcon icon={showPassword ? EyeOff : Eye} />
-          </Button>
-        }
-        {...props}
-        ref={ref}
-      />
-    );
-  },
-);
+  return (
+    <Input
+      minLength={8}
+      maxLength={30}
+      label="密码"
+      placeholder="请输入密码"
+      type={showPassword ? "text" : "password"}
+      variant="bordered"
+      endContent={
+        <Button
+          variant="light"
+          size="sm"
+          isIconOnly
+          onPress={() => {
+            setShowPassword(!showPassword);
+          }}
+        >
+          <LuIcon icon={showPassword ? EyeOff : Eye} />
+        </Button>
+      }
+      {...props}
+    />
+  );
+};
